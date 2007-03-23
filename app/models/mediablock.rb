aa795db08d94d69d57466e78161a1a44569ec5c5
Eltom <not.currently@active.com>
2007-03-23 12:50:47 +0000

    keep it DRY

--- app/models/mediablock.rb
+++ app/models/mediablock.rb
@@ -7,103 +7,34 @@ class Mediablock < ActiveRecord::Base
   def file=(incoming_file)
     @tmp_file = incoming_file
     
-    timestamp = Time.now
     self.original_name = base_part_of(incoming_file.original_filename)
     self.content_type = incoming_file.content_type.chomp
     self.title = self.original_name
-    tstmp = timestamp.to_i.to_s
-    get_extension = original_name.split('.')
-    file_extension = get_extension[get_extension.length - 1]
-    filetype = "jpg"
-    self.original = "#{tstmp}.#{file_extension}"
-    self.filename = "#{tstmp}.#{filetype}"
-    self.thumbnail = "#{tstmp}.#{filetype}"
+    manage_img self.original_name
   end
 
   def importfile=(incoming_file)
-    @importfile = incoming_file
-    
     @importflag = true
-    timestamp = Time.now
+    @importfile = incoming_file
     generate_title = original_name.split('/')
-    imported_file = generate_title[generate_title.length - 1]
+    imported_file = generate_title.last
     self.title = imported_file
-    tstmp = timestamp.to_i.to_s
-    get_extension = original_name.split('.')
-    file_extension = get_extension[get_extension.length - 1]
-    filetype = "jpg"
-    self.original = "#{tstmp}.#{file_extension}"
-    self.filename = "#{tstmp}.#{filetype}"
-    self.thumbnail = "#{tstmp}.#{filetype}"
+    manage_img original_name
 
   end
   
   def after_save
+      path = "#{RAILS_ROOT}/public/data/originals/#{self.original}"
       if @importflag
-        File.open("#{RAILS_ROOT}/public/data/originals/#{self.original}", "wb") do |f|
+        File.open(path, "wb") do |f|
           f.write( File.open(@importfile, "rb").read )
         end
-        
-        #MINI_MAGICK
-        image = MiniMagick::Image.from_file("#{RAILS_ROOT}/public/data/originals/#{self.original}")
-
-        #resize to 400X300
-        image.resize "x350"
-        #now save it with a different name
-        jpgname = "#{RAILS_ROOT}/public/data/images/#{self.filename}"
-        image.write(jpgname)
-        
-        image.resize "x150"
-        jpgname = "#{RAILS_ROOT}/public/data/thumbnails/#{self.thumbnail}"
-        image.write(jpgname)
-        
-        #RMAGICK
-        #f.write( File.read(@tmp_file) )
-        #import = Magick::Image.read(@import_file).first
-        #import.write("#{RAILS_ROOT}/public/data/originals/#{self.filename}")
-
-        #image = import.change_geometry!('x350') { |cols, rows, img|
-        #  img.resize!(cols, rows)
-        #}
-        #image.format = "JPG"
-        #image.write("#{RAILS_ROOT}/public/data/images/#{self.filename}")
-
-        #thumbnail = import.change_geometry!('x150') { |cols, rows, img|
-        #  img.resize!(cols, rows)
-        #}
-        #thumbnail.format = "JPG"
-        #thumbnail.write("#{RAILS_ROOT}/public/data/thumbnails/#{self.thumbnail}")
+        do_mini_magick path   
       else
-        File.open("#{RAILS_ROOT}/public/data/originals/#{self.original}", "wb") do |f|
+        File.open(path, "wb") do |f|
           f.write( @tmp_file.read )
         end
-        
-        #MINI_MAGICK
-        image = MiniMagick::Image.from_file("#{RAILS_ROOT}/public/data/originals/#{self.original}")
-
-        #resize to 400X300
-        image.resize "x350"
-        #now save it with a different name
-        jpgname = "#{RAILS_ROOT}/public/data/images/#{self.filename}"
-        image.write(jpgname)
-        
-        image.resize "x150"
-        jpgname = "#{RAILS_ROOT}/public/data/thumbnails/#{self.thumbnail}"
-        image.write(jpgname)
-
-        
-        #RMAGICK
-        #original = Magick::Image.read("#{RAILS_ROOT}/public/data/originals/#{self.original}").first
-        #image = original.change_geometry!('x350') { |cols, rows, img|
-        #  img.resize!(cols, rows)
-        #}
-        #image.format = "JPG"        image.write("#{RAILS_ROOT}/public/data/images/#{self.filename}")
-
-        #thumbnail = original.change_geometry!('x150') { |cols, rows, img|
-        #  img.resize!(cols, rows)
-        #}
-        #thumbnail.format = "JPG"
-        #thumbnail.write("#{RAILS_ROOT}/public/data/thumbnails/#{self.thumbnail}")
+        do_mini_magick path
     end
   end
   
@@ -134,4 +65,24 @@ private
     filename = filename.gsub(%r{^\.|[\s/\\\*\:\?'"<>\|]}, '_')
   end
   
+  def manage_img(original_name)
+    timestamp = Time.now
+    tstmp = timestamp.to_i.to_s
+    get_extension = original_name.split('.')
+    file_extension = get_extension.last
+    filetype = "jpg"
+    self.original = "#{tstmp}.#{file_extension}"
+    self.filename = "#{tstmp}.#{filetype}"
+    self.thumbnail = "#{tstmp}.#{filetype}"
+  end
+  
+  def do_mini_magick(path)
+    image = MiniMagick::Image.from_file(path)
+    image.resize "x350"
+    jpgname = "#{RAILS_ROOT}/public/data/images/#{self.filename}"
+    image.write(jpgname)
+    image.resize "x150"
+    jpgname = "#{RAILS_ROOT}/public/data/thumbnails/#{self.thumbnail}"
+    image.write(jpgname)
+  end
 end
